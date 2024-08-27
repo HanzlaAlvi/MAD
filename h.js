d7836fd6e75e6967fe887b70041b16e850154ec1
HanzlaAlvi <hanzlaalvi9@gmail.com>
2024-08-27 13:55:49 +0500

    changes

--- h.js
+++ h.js
@@ -1,1 +1,37 @@
-console.log("HELLO WORLD");
+//console.log("HELLO WORLD");
+function simple() {
+    var x = 10;
+    var y = 30;
+    console.log(x+y);
+}
+simple();
+
+
+//Anonymus function 
+let z= function sum() {
+    let x = 20;
+    let y = 50;
+    console.log(x+y);
+}
+z();
+
+//Anonymus function 
+const k= function () {
+    var x = 20;
+    var y = 50;
+    console.log(x-y);
+}
+k();
+
+function simple(x=19, y=1) {
+    console.log(x+y);
+}
+simple(11, 22);
+
+//Arrow function 
+l = () =>{
+    const x = 20;
+    const y = 50;
+    console.log(x-y);
+}bvbvff
+l();
